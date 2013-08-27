4e7a68114a50aa85bc005c1d48d466b4486b1d44
Jai Ram Rideout <jai.rideout@gmail.com>
2013-08-27 15:25:21 -0400

    Update for biom-format 1.2.0-dev

--- biom-format.sh
+++ biom-format.sh
@@ -2,10 +2,10 @@
 rm -rf qiime-deploy qiime-deploy-conf biom_format_software
 git clone git://github.com/qiime/qiime-deploy.git
 git clone git://github.com/qiime/qiime-deploy-conf.git
-sed -i -e"s?repository-location: git://github.com/biom-format/biom-format.git?repository-location: $WORKSPACE/biom-format\nlocal-repository: yes?" qiime-deploy-conf/biom-format-1.1.2-dev/biom-format.conf
-sed -i -e"s?log-level: INFO?log-level: DEBUG?" qiime-deploy-conf/biom-format-1.1.2-dev/biom-format.conf
+sed -i -e"s?repository-location: git://github.com/biom-format/biom-format.git?repository-location: $WORKSPACE/biom-format\nlocal-repository: yes?" qiime-deploy-conf/biom-format-1.2.0-dev/biom-format.conf
+sed -i -e"s?log-level: INFO?log-level: DEBUG?" qiime-deploy-conf/biom-format-1.2.0-dev/biom-format.conf
 cp -r /home/ubuntu/qiime_software biom_format_software
-python qiime-deploy/qiime-deploy.py biom_format_software -f qiime-deploy-conf/biom-format-1.1.2-dev/biom-format.conf --force-remove-failed-dirs --force-remove-previous-repos
+python qiime-deploy/qiime-deploy.py biom_format_software -f qiime-deploy-conf/biom-format-1.2.0-dev/biom-format.conf --force-remove-failed-dirs --force-remove-previous-repos
 source biom_format_software/activate.sh
 biom show-install-info
 cd biom_format_software/biom-format-*-repository-*/python-code/tests

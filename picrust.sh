c932a9ac3f71cd8361597bd5eb9a3cc16056144e
Jai Ram Rideout <jai.rideout@gmail.com>
2013-09-03 16:46:05 -0400

    Update to picrust 1.0.0-dev

--- picrust.sh
+++ picrust.sh
@@ -2,9 +2,9 @@
 rm -rf qiime-deploy qiime-deploy-conf picrust_software
 git clone git://github.com/qiime/qiime-deploy.git
 git clone git://github.com/qiime/qiime-deploy-conf.git
-sed -i -e"s?repository-location: git://github.com/picrust/picrust.git?repository-location: $WORKSPACE/picrust\nlocal-repository: yes?" qiime-deploy-conf/picrust-0.9.0-dev/picrust.conf
-sed -i -e"s?log-level: INFO?log-level: DEBUG?" qiime-deploy-conf/picrust-0.9.0-dev/picrust.conf
+sed -i -e"s?repository-location: git://github.com/picrust/picrust.git?repository-location: $WORKSPACE/picrust\nlocal-repository: yes?" qiime-deploy-conf/picrust-1.0.0-dev/picrust.conf
+sed -i -e"s?log-level: INFO?log-level: DEBUG?" qiime-deploy-conf/picrust-1.0.0-dev/picrust.conf
 cp -r /home/ubuntu/qiime_software picrust_software
-python qiime-deploy/qiime-deploy.py picrust_software -f qiime-deploy-conf/picrust-0.9.0-dev/picrust.conf --force-remove-failed-dirs --force-remove-previous-repos
+python qiime-deploy/qiime-deploy.py picrust_software -f qiime-deploy-conf/picrust-1.0.0-dev/picrust.conf --force-remove-failed-dirs --force-remove-previous-repos
 source picrust_software/activate.sh
 python picrust_software/picrust-*-repository-*/tests/all_tests.py

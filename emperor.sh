ec516120c11e7933a8d23b4922fbd51aea64f81e
Jai Ram Rideout <jai.rideout@gmail.com>
2013-12-03 16:52:57 -0700

    Update emperor script to use new qiime-deploy conf

--- emperor.sh
+++ emperor.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -ex
-source /home/ubuntu/qiime_software/activate.sh
-export PATH=$WORKSPACE/emperor/scripts:$PATH
-export PYTHONPATH=$WORKSPACE/emperor:$PYTHONPATH
-python emperor/tests/all_tests.py
+rm -rf qiime-deploy qiime-deploy-conf emperor_software
+git clone git://github.com/qiime/qiime-deploy.git
+git clone git://github.com/qiime/qiime-deploy-conf.git
+sed -i -e"s?repository-location: git://github.com/qiime/emperor.git?repository-location: $WORKSPACE/emperor\nlocal-repository: yes?" qiime-deploy-conf/emperor-0.9.2-dev/emperor.conf
+cp -r /home/ubuntu/qiime_software emperor_software
+python qiime-deploy/qiime-deploy.py emperor_software -f qiime-deploy-conf/emperor-0.9.2-dev/emperor.conf --force-remove-failed-dirs --force-remove-previous-repos
+source emperor_software/activate.sh
+cd emperor_software/emperor-*-repository-*
+python tests/all_tests.py

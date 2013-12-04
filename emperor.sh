d3336c5a4a1a67b0c54d80568ed30f32f28033a5
Jai Ram Rideout <jai.rideout@gmail.com>
2013-12-03 17:10:47 -0700

    put emperor in debug mode

--- emperor.sh
+++ emperor.sh
@@ -3,6 +3,7 @@ rm -rf qiime-deploy qiime-deploy-conf emperor_software
 git clone git://github.com/qiime/qiime-deploy.git
 git clone git://github.com/qiime/qiime-deploy-conf.git
 sed -i -e"s?repository-location: git://github.com/qiime/emperor.git?repository-location: $WORKSPACE/emperor\nlocal-repository: yes?" qiime-deploy-conf/emperor-0.9.2-dev/emperor.conf
+sed -i -e"s?log-level: INFO?log-level: DEBUG?" qiime-deploy-conf/emperor-0.9.2-dev/emperor.conf
 cp -r /home/ubuntu/qiime_software emperor_software
 python qiime-deploy/qiime-deploy.py emperor_software -f qiime-deploy-conf/emperor-0.9.2-dev/emperor.conf --force-remove-failed-dirs --force-remove-previous-repos
 source emperor_software/activate.sh

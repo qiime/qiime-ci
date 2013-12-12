b7224dc3cc988c8854809d1655247f7c05d572d4
Jai Ram Rideout <jai.rideout@gmail.com>
2013-12-12 09:45:10 -0500

    update to qiime 1.8.0-dev

--- qiime.sh
+++ qiime.sh
@@ -2,11 +2,11 @@
 rm -rf qiime-deploy qiime-deploy-conf qiime_software
 git clone git://github.com/qiime/qiime-deploy.git
 git clone git://github.com/qiime/qiime-deploy-conf.git
-sed -i -e"s?repository-location: git://github.com/qiime/qiime.git?repository-location: $WORKSPACE/qiime\nlocal-repository: yes?" qiime-deploy-conf/qiime-1.7.0-dev/qiime.conf
-sed -i -e"s?log-level: INFO?log-level: DEBUG?" qiime-deploy-conf/qiime-1.7.0-dev/qiime.conf
+sed -i -e"s?repository-location: git://github.com/qiime/qiime.git?repository-location: $WORKSPACE/qiime\nlocal-repository: yes?" qiime-deploy-conf/qiime-1.8.0-dev/qiime.conf
+sed -i -e"s?log-level: INFO?log-level: DEBUG?" qiime-deploy-conf/qiime-1.8.0-dev/qiime.conf
 cp -r /home/ubuntu/qiime_software .
 rm -rf qiime_software/*-latest-r-package
-python qiime-deploy/qiime-deploy.py qiime_software -f qiime-deploy-conf/qiime-1.7.0-dev/qiime.conf --force-remove-failed-dirs --force-remove-previous-repos
+python qiime-deploy/qiime-deploy.py qiime_software -f qiime-deploy-conf/qiime-1.8.0-dev/qiime.conf --force-remove-failed-dirs --force-remove-previous-repos
 sed -i -e's/97_otus.fasta/61_otus.fasta/' qiime_software/qiime_config
 sed -i -e's/97_otu_taxonomy.txt/61_otu_taxonomy.txt/' qiime_software/qiime_config
 source qiime_software/activate.sh

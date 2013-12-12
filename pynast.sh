56e327f8d0b3e452b6a809d0033fe3755de87293
Jai Ram Rideout <jai.rideout@gmail.com>
2013-12-12 10:26:41 -0500

    update to use new qiime-deploy-conf dir structure

--- pynast.sh
+++ pynast.sh
@@ -2,9 +2,9 @@
 rm -rf qiime-deploy qiime-deploy-conf pynast_software
 git clone git://github.com/qiime/qiime-deploy.git
 git clone git://github.com/qiime/qiime-deploy-conf.git
-sed -i -e"s?repository-location: git://github.com/qiime/pynast.git?repository-location: $WORKSPACE/pynast\nlocal-repository: yes?" qiime-deploy-conf/pynast-1.2.2-dev/pynast.conf
+sed -i -e"s?repository-location: git://github.com/qiime/pynast.git?repository-location: $WORKSPACE/pynast\nlocal-repository: yes?" qiime-deploy-conf/pynast-dev/pynast.conf
 cp -r /home/ubuntu/qiime_software pynast_software
-python qiime-deploy/qiime-deploy.py pynast_software -f qiime-deploy-conf/pynast-1.2.2-dev/pynast.conf --force-remove-failed-dirs --force-remove-previous-repos
+python qiime-deploy/qiime-deploy.py pynast_software -f qiime-deploy-conf/pynast-dev/pynast.conf --force-remove-failed-dirs --force-remove-previous-repos
 source pynast_software/activate.sh
 export PATH=/home/ubuntu/jenkins-support:/home/ubuntu/jenkins-support/mafft/bin:$PATH
 export MAFFT_BINARIES=/home/ubuntu/jenkins-support/mafft/lib/mafft

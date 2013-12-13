333a37bfd3f34d9793caa18e0af1061bb96a5e3a
Jai Ram Rideout <jai.rideout@gmail.com>
2013-12-13 11:05:06 -0500

    fix r package stuff related to ami upgrade

--- qiime.sh
+++ qiime.sh
@@ -5,7 +5,6 @@ git clone git://github.com/qiime/qiime-deploy-conf.git
 sed -i -e"s?repository-location: git://github.com/qiime/qiime.git?repository-location: $WORKSPACE/qiime\nlocal-repository: yes?" qiime-deploy-conf/qiime-dev/qiime.conf
 sed -i -e"s?log-level: INFO?log-level: DEBUG?" qiime-deploy-conf/qiime-dev/qiime.conf
 cp -r /home/ubuntu/qiime_software .
-rm -rf qiime_software/*-latest-r-package
 python qiime-deploy/qiime-deploy.py qiime_software -f qiime-deploy-conf/qiime-dev/qiime.conf --force-remove-failed-dirs --force-remove-previous-repos
 sed -i -e's/97_otus.fasta/61_otus.fasta/' qiime_software/qiime_config
 sed -i -e's/97_otu_taxonomy.txt/61_otu_taxonomy.txt/' qiime_software/qiime_config

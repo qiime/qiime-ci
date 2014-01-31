6ea5272016f33c808c06b72ab123dad2ebec49b6
Jai Ram Rideout <jai.rideout@gmail.com>
2014-01-31 15:55:14 -0500

    update qiime script to account for pip installation

--- qiime.sh
+++ qiime.sh
@@ -2,13 +2,13 @@
 rm -rf qiime-deploy qiime-deploy-conf qiime_software
 git clone git://github.com/qiime/qiime-deploy.git
 git clone git://github.com/qiime/qiime-deploy-conf.git
-sed -i -e"s?repository-location: git://github.com/qiime/qiime.git?repository-location: $WORKSPACE/qiime\nlocal-repository: yes?" qiime-deploy-conf/qiime-dev/qiime.conf
-sed -i -e"s?log-level: INFO?log-level: DEBUG?" qiime-deploy-conf/qiime-dev/qiime.conf
 cp -r /home/ubuntu/qiime_software .
-python qiime-deploy/qiime-deploy.py qiime_software -f qiime-deploy-conf/qiime-dev/qiime.conf --force-remove-failed-dirs --force-remove-previous-repos
+pip install --quiet "numpy>=1.5.1,<=1.7.1"
+pip install --quiet $WORKSPACE/qiime
+$PYTHON_EXE qiime-deploy/qiime-deploy.py qiime_software -f qiime-deploy-conf/qiime-dev/qiime.conf --force-remove-failed-dirs --force-remove-previous-repos
 sed -i -e's/97_otus.fasta/61_otus.fasta/' qiime_software/qiime_config
 sed -i -e's/97_otu_taxonomy.txt/61_otu_taxonomy.txt/' qiime_software/qiime_config
 source qiime_software/activate.sh
 export PATH=/home/ubuntu/jenkins-support:$PATH
-print_qiime_config.py
-python qiime_software/qiime-*-repository-*/tests/all_tests.py
+print_qiime_config.py -t
+$PYTHON_EXE $WORKSPACE/qiime/tests/all_tests.py

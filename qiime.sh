6071f4377f247b37e2113333dd77b7126038978c
Jai Ram Rideout <jai.rideout@gmail.com>
2014-02-04 13:45:42 -0500

    debugging done; adding back --quiet flag

--- qiime.sh
+++ qiime.sh
@@ -4,7 +4,7 @@ git clone git://github.com/qiime/qiime-deploy.git
 git clone git://github.com/qiime/qiime-deploy-conf.git
 cp -r /home/ubuntu/qiime_software .
 pip install --quiet "numpy>=1.5.1,<=1.7.1"
-pip install $WORKSPACE/qiime
+pip install --quiet $WORKSPACE/qiime
 $PYTHON_EXE qiime-deploy/qiime-deploy.py qiime_software -f qiime-deploy-conf/qiime-dev/qiime.conf --force-remove-failed-dirs --force-remove-previous-repos
 sed -i -e's/97_otus.fasta/61_otus.fasta/' qiime_software/qiime_config
 sed -i -e's/97_otu_taxonomy.txt/61_otu_taxonomy.txt/' qiime_software/qiime_config
